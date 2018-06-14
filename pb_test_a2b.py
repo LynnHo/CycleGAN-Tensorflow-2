400152af21ef14d6d933d0ab768ef209fde5834d
Andrew Ginns <ginns.aw@gmail.com>
2018-06-14 17:13:38 +0100

    New workflow
    Freeze the graph through freeze.py
    - python freeze.py path/to/ckpts /tmp/frozen_graph.pb
    
    Optimise using
    - bazel-bin/tensorflow/python/tools/optimize_for_inference --input=/tmp/frozen_graph.pb --output=/tmp/optimized_cycle_graph.pb --frozen_graph=True --input_names=inputA --output_names=a2b_generator/output_image
    
    Test (in progress)
    python load_graph.py
    python pb_test_a2b.py

---
+++ pb_test_a2b.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import, division, print_function
+
+import argparse
+from glob import glob
+import os
+
+import image_utils as im
+import models
+import numpy as np
+import tensorflow as tf
+import utils
+
+
+""" param """
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--dataset', dest='dataset', default='summer2winter_yosemite', help='which dataset to use')
+parser.add_argument('--crop_size', dest='crop_size', type=int, default=256, help='then crop to this size')
+args = parser.parse_args()
+
+dataset = args.dataset
+crop_size = args.crop_size
+
+# restore
+model_file = "/tmp/optimized_cycle_graph.pb"
+
+graph = tf.Graph()
+graph_def = tf.GraphDef()
+
+with open(model_file, "rb") as f:
+    graph_def.ParseFromString(f.read())
+with graph.as_default():
+    tf.import_graph_def(graph_def)
+
+
+""" run """
+with tf.Session(graph=graph) as sess:
+    a_real = tf.placeholder(tf.float32, shape=[None, crop_size, crop_size, 3])
+
+    a2b = models.generator(a_real, 'a2b')
+
+    # test
+    a_list = glob('./datasets/' + dataset + '/testA/*.jpg')
+
+    a_save_dir = './outputs/test_predictions/' + dataset + '/testA'
+    utils.mkdir([a_save_dir])
+
+    for i in range(len(a_list)):
+        a_real_ipt = im.imresize(im.imread(a_list[i]), [crop_size, crop_size])
+        a_real_ipt.shape = 1, crop_size, crop_size, 3
+        a2b_opt = sess.run([a2b], feed_dict={a_real: a_real_ipt})
+        a_img_opt = np.concatenate(a2b_opt, axis=0)
+
+        img_name = os.path.basename(a_list[i])
+        im.imwrite(im.immerge(a_img_opt, 1, 1), a_save_dir + '/' + img_name)
+        print('Save %s' % (a_save_dir + '/' + img_name))
