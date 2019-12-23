ed5b0df084c447b1bd772854e26486536e037935
gengxqa <gengxiaoqi@singularity-hl.ai>
2019-12-23 19:59:40 +0800

    test git

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #test1:data=10,size=1024
-CUDA_VISIBLE_DEVICES=4,5 python ./test.py --experiment_dir '/mnt/share/gengxiaoqi/2cycleGAN/output_test2/wsi_pair_patch' --samples_testing 'samples_testing3' --testA '4testA' --testB '4testB' --load_size 512 --crop_size 512
+CUDA_VISIBLE_DEVICES=4,5 python ./test.py --experiment_dir '/mnt/share/gengxiaoqi/2cycleGAN/output_test3/wsi_pair_patch' --samples_testing 'samples_testing_lichao2' --testA '8testA' --testB '8testB' --load_size 512 --crop_size 512
